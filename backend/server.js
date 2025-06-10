e11e75f7b0a2f698e1f13bb2398382a2478fe230
shraddha agnihotri <shraddhaagnihotri529@gmail.com>
2025-06-10 12:36:48 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // API routes
